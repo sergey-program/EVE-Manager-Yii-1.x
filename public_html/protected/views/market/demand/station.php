ed08209a08c1f07b9e7f610fdca46e6acb4a84eb
sergey-program <sergey.xz01d@gmail.com>
2014-06-13 10:01:05 -0400

    Fix, buy order preview

--- public_html/protected/views/market/demand/station.php
+++ public_html/protected/views/market/demand/station.php
@@ -83,9 +83,57 @@
                 <h1>Buy</h1>
                 <ul class="list-group">
                     <?php foreach ($cStation->getDemands($cCharacter->getCharacterID(), clDemand::TYPE_BUY) as $cDemand): ?>
+                        <li class="list-group-item list-group-item-success">
+                            <div class="pull-right line-height-42">Need: <strong><?php echo $cDemand->getNeed($cCharacter->getCharacterID()); ?></strong> Demand: <?php echo $cDemand->getQuantity(); ?></div>
+                            <img class="img-thumbnail" src="http://image.eveonline.com/Type/<?php echo $cDemand->getTypeID(); ?>_32.png">
+                            <span class="margin-left-15"><?php echo $cDemand->getTypeName(); ?></span>
+                        </li>
                         <li class="list-group-item">
-                            <img class="img-thumbnail margin-right-15" src="http://image.eveonline.com/Type/<?php echo $cDemand->getTypeID(); ?>_32.png">
-                            <?php echo $cDemand->getTypeName(); ?>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <ul class="list-group">
+                                        <li class="list-group-item">
+                                            <span>Jita Buy:</span>
+                                            <div class="pull-right"><?php echo number_format($cDemand->getPriceBuy(), 2, ',', ' '); ?></div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span>Jita Sell:</span>
+                                            <div class="pull-right"><?php echo number_format($cDemand->getPriceSell(), 2, ',', ' '); ?></div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span>My Buy:</span>
+                                            <div class="pull-right">
+                                                <span class="text-muted"><small>-<?php echo cPrice::getPercentSell(); ?>%</small></span>
+                                                <?php echo number_format($cDemand->getPriceBuy(true), 2, ',', ' '); ?>
+                                            </div>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span>My Sell:</span>
+                                            <div class="pull-right">
+                                                <span class="text-muted"><small>+<?php echo cPrice::getPercentBuy(); ?>%</small></span>
+                                                <?php echo number_format($cDemand->getPriceSell(true), 2, ',', ' '); ?>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <!-- col -->
+                                <div class="col-md-6">
+                                    <ul class="list-group margin-bottom-0">
+                                        <?php if ($cDemand->getOrders($cCharacter->getCharacterID())): ?>
+                                            <?php foreach ($cDemand->getOrders($cCharacter->getCharacterID()) as $cOrder): ?>
+                                                <li class="list-group-item list-group-item-info">
+                                                    <div class="pull-right"><?php echo $cOrder->getVolRemaining(); ?> / <?php echo $cOrder->getVolEntered(); ?></div>
+                                                    <span>#<?php echo $cOrder->getOrderID(); ?></span>
+                                                </li>
+                                            <?php endforeach; ?>
+                                        <?php else: ?>
+                                            <li class="list-group-item list-group-item-danger text-center">no orders</li>
+                                        <?php endif; ?>
+                                    </ul>
+                                </div>
+                                <!-- col -->
+                            </div>
+                            <!-- row -->
                         </li>
                     <?php endforeach; ?>
                 </ul>
